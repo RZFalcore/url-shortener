c0fb72e0615df748eb9c197b55860f0ae5d09921
Eugene Roy <box.rz.box@gmail.com>
2021-08-06 17:22:13 +0300

    registration logic added

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,54 @@
 const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 const router = Router();
 
 // /api/auth
 
-router.get("/register", async (req, res) => {
+router.get(
+  "/registration",
+  [
+    check("email", "Invalid email").isEmail(),
+    check("password", "Password must be at least 6 chars.").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Invalid registration data",
+        });
+
+      const { email, password } = req.body;
+
+      const isExist = await User.findOne({ email });
+      if (isExist)
+        return res.status(400).json({ message: "This user is already exist." });
+
+      const hashedPassword = await bcrypt.hash(password, 6);
+      const user = new User({ email, password: hashedPassword });
+      await user.save();
+
+      res.status(201).json({ message: "User created!" });
+    } catch (error) {
+      res.status(500).json({ message: "Something go wrong..." });
+    }
+  }
+);
+
+router.get("/login", async (req, res) => {
   try {
-    const { login, password } = req.body;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(400).json({errors: })
+
   } catch (error) {
     res.status(500).json({ message: "Something go wrong..." });
   }
-});
-
-router.get("/login", async (req, res) => {
-  //
 });
 
 module.exports = router;
