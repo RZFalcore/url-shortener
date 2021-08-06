6bb972041bfe9788909bcb9ced5fe61b78592ba3
Eugene Roy <box.rz.box@gmail.com>
2021-08-06 16:36:09 +0300

    User model added

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,11 @@ const router = Router();
 // /api/auth
 
 router.get("/register", async (req, res) => {
-  //
+  try {
+    const { login, password } = req.body;
+  } catch (error) {
+    res.status(500).json({ message: "Something go wrong..." });
+  }
 });
 
 router.get("/login", async (req, res) => {
